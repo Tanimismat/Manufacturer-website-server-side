715ae83fd2084bd2bee49d6a4a44415d01a90ff8
Tanim Ismat <tanim.ismat@gmail.com>
2022-07-04 11:36:12 +0600

    Token generated when user created

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oid2i.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,6 +22,7 @@ async function run() {
         const database = client.db('manufacturer_website');
         const toolCollection = database.collection('tools');
         const orderCollection = database.collection('orders');
+        const userCollection = database.collection('users');
         
         // loading data from database
         app.get('/tools', async (req, res) => {
@@ -50,6 +52,19 @@ async function run() {
             const query = { user: user };
             const orders = await orderCollection.find(query).toArray();
             res.send(orders)
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user ,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+            res.send({result, token});
         })
     } 
     finally { 
